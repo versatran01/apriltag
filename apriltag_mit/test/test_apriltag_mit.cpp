67beef1790315f6052ba81a23834569d3ef070b0
Chao Qu <quchao@seas.upenn.edu>
2015-03-20 19:55:47 -0400

    refactored apritlag_mit unittest to use value-parameterized test

--- apriltag_mit/test/test_apriltag_mit.cpp
+++ apriltag_mit/test/test_apriltag_mit.cpp
@@ -18,49 +18,21 @@ class SampleImageTest : public testing::Test {
   cv::Mat test_image_;
 };
 
-TEST_F(SampleImageTest, Tag36h11) {
-  TagDetector tag_detector(tagCodes36h11);
+class TagCodesTest : public SampleImageTest,
+                     public ::testing::WithParamInterface<TagCodes> {};
+
+TEST_P(TagCodesTest, Detection) {
+  TagDetector tag_detector(GetParam());
   std::vector<TagDetection> tag_detection =
       tag_detector.extractTags(test_image_);
-  ASSERT_EQ(4, tag_detection.size());
+  EXPECT_EQ(4, tag_detection.size());
 }
+
+INSTANTIATE_TEST_CASE_P(ThreeTagCodes, TagCodesTest,
+                        ::testing::Values(tagCodes36h11, tagCodes25h9,
+                                          tagCodes16h5));
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
-// Old stuff
-// int main(int argc, char** argv) {
-//  const std::string package_name = "apriltag_mit";
-//  const std::string package_path(ros::package::getPath(package_name));
-//  const std::string image_path(package_path + "/image/tag_sampler.png");
-//  std::cout << "Image: " << image_path << std::endl;
-
-//  // Load image
-//  cv::Mat image = cv::imread(image_path, CV_LOAD_IMAGE_COLOR);
-//  cv::imshow("image", image);
-
-//  // Process image
-//  cv::Mat image_gray;
-//  cv::cvtColor(image, image_gray, CV_BGR2GRAY);
-
-//  // There are three tag families in this image
-//  std::vector<TagCodes> all_tag_codes = {tagCodes36h11, tagCodes25h9,
-//                                         tagCodes16h5};
-
-//  for (const TagCodes& tag_codes : all_tag_codes) {
-//    TagDetector tag_detector(tag_codes);
-//    std::vector<TagDetection> tag_detection =
-//        tag_detector.extractTags(image_gray);
-//    std::cout << "Number of detections: " << tag_detection.size() <<
-//    std::endl;
-//    for (const TagDetection& tag : tag_detection) {
-//      std::cout << tag.p[0].first << " " << tag.p[0].second << std::endl;
-//      tag.draw(image);
-//    }
-//  }
-
-//  cv::imshow("detection", image);
-//  cv::waitKey(-1);
-//}
