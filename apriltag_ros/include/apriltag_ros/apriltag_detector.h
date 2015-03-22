982073720a563a8a1705a270625703862e071ad4
Chao Qu <quchao@seas.upenn.edu>
2015-03-21 22:02:01 -0400

    some reorganization

---
+++ apriltag_ros/include/apriltag_ros/apriltag_detector.h
@@ -0,0 +1,18 @@
+#ifndef APRILTAG_ROS_APRILTAG_DETECTOR_H_
+#define APRILTAG_ROS_APRILTAG_DETECTOR_H_
+
+namespace apriltag_ros {
+
+class ApriltagDetectorBase {
+ public:
+ protected:
+ private:
+};
+
+class ApriltagDetectorMit : ApriltagDetectorBase {};
+
+class ApriltagDetectorUmich : ApriltagDetectorBase {};
+
+}  // apriltag_ros
+
+#endif  // APRILTAG_ROS_APRILTAG_DETECTOR_H_
