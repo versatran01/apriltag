4ce92391d1d1b3ab4ab975846b9bcbc677855744
Chao Qu <quchao@seas.upenn.edu>
2015-12-23 13:21:48 -0500

    update

--- apriltag_ros/test/playground.cpp
+++ apriltag_ros/test/playground.cpp
@@ -7,12 +7,10 @@ using namespace apriltag_ros;
 int main() {
   const std::string package_name("apriltag_ros");
   const std::string package_path(ros::package::getPath(package_name));
-  const std::string image_path(package_path + "/image/tag_sampler.png");
+  const std::string image_path("/home/chao/Downloads/test.png");
   cv::Mat image = cv::imread(image_path, CV_LOAD_IMAGE_COLOR);
 
-  ApriltagDetectorPtr detector = ApriltagDetector::create("mit", "36h11");
-  detector->set_decimate(2);
-  detector->set_refine(true);
+  ApriltagDetectorPtr detector = ApriltagDetector::create("mit", "t36h11");
   detector->detect(image);
   detector->draw(image);
   cv::namedWindow("image", CV_WINDOW_KEEPRATIO | CV_WINDOW_NORMAL);
