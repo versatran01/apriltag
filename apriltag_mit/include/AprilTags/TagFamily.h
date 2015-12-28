c76a84f167f9872066049805f5ae731f43309984
Chao Qu <quchao@seas.upenn.edu>
2015-12-28 15:26:44 -0500

    use builtin popcount

--- apriltag_mit/include/AprilTags/TagFamily.h
+++ apriltag_mit/include/AprilTags/TagFamily.h
@@ -25,34 +25,9 @@ class TagFamily {
   void set_error_recovery_bits(unsigned error_recovery_bits);
   void set_error_recovery_fraction(float v);
 
-  //! Computes the hamming distance between two code_ts.
-  static int hammingDistance(code_t a, code_t b);
-
-  //! How many bits are set in the code_t?
-  static unsigned char popCountReal(code_t w);
-
-  static int popCount(code_t w);
-
   //! Given an observed tag with code 'rCode', try to recover the id.
   /*  The corresponding fields of TagDetection will be filled in. */
   void decode(TagDetection& det, code_t rCode) const;
-
-  void printHammingDistances() const;
-  //! Numer of pixels wide of the inner black border.
-  //  int blackBorder;
-
-  static const int popCountTableShift = 12;
-  static const unsigned int popCountTableSize = 1 << popCountTableShift;
-  static unsigned char popCountTable[popCountTableSize];
-
-  //! Initializes the static popCountTable
-  static class TableInitializer {
-   public:
-    TableInitializer() {
-      for (unsigned int i = 0; i < TagFamily::popCountTableSize; i++)
-        TagFamily::popCountTable[i] = TagFamily::popCountReal(i);
-    }
-  } initializer;
 
  private:
   const TagCodes& tag_codes_;
@@ -76,7 +51,20 @@ class TagFamily {
  *  5 4 3  ==>  1 4 7 ==>  3 4 5    (rotate90 applied twice)
  *  2 1 0       0 3 6      6 7 8
  */
-code_t rotate90(code_t w, int d);
+code_t rotate90_cwise(code_t w, int d);
+
+/**
+ * @brief hamming_distance
+ * @param a
+ * @param b
+ */
+unsigned hamming_distance(code_t a, code_t b);
+
+/**
+ * @brief popCount
+ * @param w
+ */
+unsigned pop_count(code_t w);
 
 }  // namespace AprilTags
 
--- apriltag_mit/src/TagFamily.cc
+++ apriltag_mit/src/TagFamily.cc
@@ -26,7 +26,6 @@ namespace AprilTags {
 
 using namespace std;
 
-
 TagFamily::TagFamily(const TagCodes &tag_codes)
     : tag_codes_(tag_codes),
       num_codes_(tag_codes.codes.size()),
@@ -45,26 +44,6 @@ void TagFamily::set_error_recovery_fraction(float v) {
   error_recovery_bits_ = static_cast<unsigned>(((min_hamming() - 1) / 2) * v);
 }
 
-int TagFamily::hammingDistance(code_t a, code_t b) { return popCount(a ^ b); }
-
-unsigned char TagFamily::popCountReal(code_t w) {
-  unsigned char cnt = 0;
-  while (w != 0) {
-    w &= (w - 1);
-    ++cnt;
-  }
-  return cnt;
-}
-
-int TagFamily::popCount(code_t w) {
-  int count = 0;
-  while (w != 0) {
-    count += popCountTable[(unsigned int)(w & (popCountTableSize - 1))];
-    w >>= popCountTableShift;
-  }
-  return count;
-}
-
 void TagFamily::decode(TagDetection &det, code_t rCode) const {
   int bestId = -1;
   int bestHamming = INT_MAX;
@@ -73,13 +52,13 @@ void TagFamily::decode(TagDetection &det, code_t rCode) const {
 
   code_t rCodes[4];
   rCodes[0] = rCode;
-  rCodes[1] = rotate90(rCodes[0], dimension_bits());
-  rCodes[2] = rotate90(rCodes[1], dimension_bits());
-  rCodes[3] = rotate90(rCodes[2], dimension_bits());
+  rCodes[1] = rotate90_cwise(rCodes[0], dimension_bits());
+  rCodes[2] = rotate90_cwise(rCodes[1], dimension_bits());
+  rCodes[3] = rotate90_cwise(rCodes[2], dimension_bits());
 
   for (unsigned int id = 0; id < codes().size(); id++) {
     for (unsigned int rot = 0; rot < 4; rot++) {
-      int thisHamming = hammingDistance(rCodes[rot], codes()[id]);
+      int thisHamming = hamming_distance(rCodes[rot], codes()[id]);
       if (thisHamming < bestHamming) {
         bestHamming = thisHamming;
         bestRotation = rot;
@@ -96,31 +75,7 @@ void TagFamily::decode(TagDetection &det, code_t rCode) const {
   det.code = bestCode;
 }
 
-void TagFamily::printHammingDistances() const {
-  vector<int> hammings(dimension_bits() * dimension_bits() + 1);
-  for (unsigned i = 0; i < codes().size(); i++) {
-    code_t r0 = codes()[i];
-    code_t r1 = rotate90(r0, dimension_bits());
-    code_t r2 = rotate90(r1, dimension_bits());
-    code_t r3 = rotate90(r2, dimension_bits());
-    for (unsigned int j = i + 1; j < codes().size(); j++) {
-      int d = min(
-          min(hammingDistance(r0, codes()[j]), hammingDistance(r1, codes()[j])),
-          min(hammingDistance(r2, codes()[j]),
-              hammingDistance(r3, codes()[j])));
-      hammings[d]++;
-    }
-  }
-
-  for (unsigned int i = 0; i < hammings.size(); i++)
-    printf("hammings: %u = %d\n", i, hammings[i]);
-}
-
-unsigned char TagFamily::popCountTable[TagFamily::popCountTableSize];
-
-TagFamily::TableInitializer TagFamily::initializer;
-
-code_t rotate90(code_t w, int d) {
+code_t rotate90_cwise(code_t w, int d) {
   code_t wr = 0;
   const code_t oneLongLong = 1;
 
@@ -133,6 +88,11 @@ code_t rotate90(code_t w, int d) {
     }
   }
   return wr;
+}
+
+unsigned hamming_distance(code_t a, code_t b) {
+  // Because code_t is unsigned long long
+  return __builtin_popcountll(a ^ b);
 }
 
 }  // namespace AprilTags
