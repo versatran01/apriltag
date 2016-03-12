37c2b1d137a60d31989a2de35a95cd8b320e9936
Chao Qu <quchao@seas.upenn.edu>
2016-03-11 23:37:06 -0500

    add some stuff

--- apriltag_py/test.py
+++ apriltag_py/test.py
@@ -8,26 +8,49 @@ import matplotlib.pyplot as plt
 # %%
 
 
-def imshow(image, cm=None, title=None, figsize=None):
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(111)
-    ax.imshow(image, cmap=cm)
-    ax.set_title(title)
+def imshow(*args, **kwargs):
+    n = len(args)
+    figsize = kwargs.get('figsize', (7.5, 7.5))
+    cmap = kwargs.get('cmap', plt.cm.gray)
+    title = kwargs.get('title', [])
+    if isinstance(title, basestring):
+        title = [title]
 
+    f, axes = plt.subplots(nrows=1, ncols=n, figsize=figsize)
+    axes = np.atleast_1d(axes)
+    for ax, im in zip(axes, args):
+        ax.imshow(im, cmap=cmap)
+
+    for ax, t in zip(axes, title):
+        ax.set_title(t)
+    return np.ravel(axes)
 
 # %%
 cwd = os.getcwd()
-image = cv2.imread(os.path.join(cwd, 'frame0000.jpg'))
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-imshow(image, title='original')
+image_file = os.path.join(cwd, 'frame0001.jpg')
+color = cv2.imread(image_file, cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+imshow(color, title='raw')
 
 # %%
-# Gaussian blur
-sigma = 0.8
-ksize = 3
-gray_blur = cv2.GaussianBlur(gray, (ksize, ksize), sigma)
-gray_diff = gray - gray_blur
-imshow(gray_blur, cm=plt.cm.gray, title='blur')
-imshow(gray_diff, cm=plt.cm.gray, title='noise')
+# Gaussian blur with downsample
+gray_sample = gray
+gray_seg = cv2.pyrDown(gray)
+imshow(gray_seg, title='seg')
 
 # %%
+# Calculate image gradient
+scale = 1
+Ix = cv2.Scharr(gray_seg, cv2.CV_32F, 1, 0, scale=scale)
+Iy = cv2.Scharr(gray_seg, cv2.CV_32F, 0, 1, scale=scale)
+ax1, ax2 = imshow(Ix, Iy, figsize=(16, 10), title=('Ix', 'Iy'))
+
+# %%
+# Calculate gradient magnitude and angle
+im_mag, im_ang = cv2.cartToPolar(Ix, Iy)
+imshow(im_mag, im_ang, figsize=(16, 10), title=('mag', 'ang'))
+print('max mag:', np.max(im_mag))
+
+f, ax = plt.subplots(figsize=(12, 3))
+ax.hist(im_mag.ravel(), 255)
+plt.show()
